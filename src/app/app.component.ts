d582e7a6867d8dec75c106918b6194bb9cd369f4
huyhq@doctoctella.com <huyhq@doctoctella.com>
2018-01-31 17:54:02 +0700

    add jquery

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewChecked } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewChecked {
     title = 'Tour of Heroes';
+
+    ngAfterViewChecked() {
+        $('#app-content').show();
+        $('.loading').hide();
+    }
 }
